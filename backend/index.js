a99348136b181424073c29dde656ae548319d927
Anurag123-cloud <anuragmaurya566@gmail.com>
2024-10-14 11:36:02 +0530

    project

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ const port = process.env.PORT || 8000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin:'https://doctor-appointment-website-zmgw.vercel.app',
+  origin:process.env.CLIENT_SITE_URL,
   credentials: true,
 }));
 
