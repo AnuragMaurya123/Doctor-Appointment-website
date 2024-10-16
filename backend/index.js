82a1effc250ca05cc6eb2e994c7458f26bd4b2da
Anurag123-cloud <anuragmaurya566@gmail.com>
2024-10-17 01:17:49 +0530

    changes done in project

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ const port = process.env.PORT || 8000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: [ 'http://localhost:5199', 'https://doctor-appointment-website-wxrj.vercel.app' ],
+  origin: '*',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-csrf-token'], 
