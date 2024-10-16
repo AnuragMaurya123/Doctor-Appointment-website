f768773db8322c7b48a79f087192a1d6b69b36c9
Anurag123-cloud <anuragmaurya566@gmail.com>
2024-10-16 19:06:59 +0530

    changes done in project

--- frontend/src/utils/BaseUrl.js
+++ frontend/src/utils/BaseUrl.js
@@ -1,1 +1,1 @@
-export const BACKEND_URL=import.meta.env.VITE_BACKEND_URL || "https://doctor-appointment-website-pi.vercel.app"
+export const BACKEND_URL=import.meta.env.VITE_BACKEND_URL || "https://doctor-appointment-website-rust.vercel.app"
